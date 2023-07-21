48dda8a6901442845c1720fa5f81cb2e61239e69
mousab-issa <59908473+mousab-issa@users.noreply.github.com>
2023-07-21 16:34:29 +0700

    feat: adding front-end

--- src/components/TracksGrid.jsx
+++ src/components/TracksGrid.jsx
@@ -23,7 +23,7 @@ const TracksGrid = () => {
       {trackTypeGroups.map((group, index) => (
         <div key={index}>
           <h2 className="font-bold text-2xl mb-4">{group.type}</h2>
-          <div className="grid grid-cols-3 gap-4">
+          <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
             {group.tracks.map((track) => (
               <div
                 key={track._id}
